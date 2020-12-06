3316debd973294fc4ada636830bc9b8f6a98dde0
Nick <programmernick@gmail.com>
2020-12-06 14:32:01 +0300

    Update Configuration.php
    adaptive to symfony5

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,8 +9,13 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('monolog_injection');
+        if (\Symfony\Component\HttpKernel\Kernel::MAJOR_VERSION > 4) {
+            $treeBuilder = new TreeBuilder('monolog_injection');
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $treeBuilder = new TreeBuilder();
+            $rootNode = $treeBuilder->root('monolog_injection');
+        }
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
